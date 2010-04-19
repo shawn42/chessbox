d0fd7db0d3ec1ea8a87414ddb066708b6103cc90
Shawn Anderson <shawn42@gmail.com>
2010-04-19 13:38:10 -0400

    moved spatial stagehand to gamebox core

--- src/play_stage.rb
+++ src/play_stage.rb
@@ -1,8 +1,8 @@
 class PlayStage < Stage
   def setup
     super
-    spawn :stary_night
-    spawn :stary_night, :speed => 30 
+    spawn :stary_night, :speed => 280
+    spawn :stary_night, :speed => 110 
     spawn :fps, :x => 200, :y => 30
     @board = spawn :chess_board, :x => 150, :y => 100
     @board.setup_default
